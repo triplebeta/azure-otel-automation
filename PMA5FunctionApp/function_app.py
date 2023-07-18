5ee08ea250953ac3bb5309eb4a8b652866cdfbb8
Jeroen <jeroenbijleveld@hotmail.com>
2023-07-18 19:43:22 +0200

    Added logging metrics, added exception logging, added using a context

--- PMA5FunctionApp/function_app.py
+++ PMA5FunctionApp/function_app.py
@@ -2,10 +2,41 @@ import datetime
 import logging
 import azure.functions as func
 
+# Configure OpenCensus for the logging to ApplicationInsights
+from opencensus.ext.azure import metrics_exporter
 from opencensus.extension.azure.functions import OpenCensusExtension
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+
 logger = logging.getLogger('HttpTriggerLogger')
+logger.addHandler(AzureLogHandler())
 OpenCensusExtension.configure()
 
+stats = stats_module.stats
+view_manager = stats.view_manager
+stats_recorder = stats.stats_recorder
+
+prompt_measure = measure_module.MeasureInt("prompts",
+                                           "number of prompts",
+                                           "prompts")
+prompt_view = view_module.View("prompt view",
+                               "number of prompts",
+                               [],
+                               prompt_measure,
+                               aggregation_module.CountAggregation())
+view_manager.register_view(prompt_view)
+mmap = stats_recorder.new_measurement_map()
+tmap = tag_map_module.TagMap()
+
+exporter = metrics_exporter.new_metrics_exporter()
+# Alternatively manually pass in the connection_string
+# exporter = metrics_exporter.new_metrics_exporter(connection_string='<appinsights-connection-string>')
+
+view_manager.register_exporter(exporter)
 
 app = func.FunctionApp()
 
@@ -19,11 +50,30 @@ app = func.FunctionApp()
 #   - package name
 @app.route(route="EventsGBRFake", auth_level=func.AuthLevel.ANONYMOUS)
 def EventsGBRFake(req: func.HttpRequest, context) -> func.HttpResponse:
-    logger = logging.getLogger('HttpTriggerLogger')
 
     # You must use context.tracer to create spans
     with context.tracer.span("parent"):
         logger.info('Message from HttpTrigger using the OpenCensus logger.info using a context.')
+
+    # See also: https://learn.microsoft.com/en-us/azure/azure-monitor/app/opencensus-python
+    # Use properties in exception logs
+    properties = {'custom_dimensions': {'key_1': 'value_1', 'key_2': 'value_2'}}
+    try:
+        result = 1 / 0  # generate a ZeroDivisionError
+    except Exception:
+        logger.exception('Captured an exception.', extra=properties)
+
+    # Send a customEvent. This requires that the AzureHandler is installed.
+    logger.setLevel(logging.INFO)
+    logger.info('Hello World Custom Event!')
+
+    # Show metrics 
+    for _ in range(4):
+        mmap.measure_int_put(prompt_measure, 1)
+        mmap.record(tmap)
+#          the next line fails...
+        metrics = list(mmap.measure_to_view_map.get_metrics(datetime.datetime.utcnow()))
+        print(metrics[0].time_series[0].points[0])
 
 #    logging.info('Test if info shows up in AppInsights.')
 #    logging.warning('Test if a warning shows up in AppInsights');
