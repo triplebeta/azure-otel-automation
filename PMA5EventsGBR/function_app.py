03c5550b2036bcf0b9c2964397763ac98b6e96dd
Jeroen <jeroenbijleveld@hotmail.com>
2023-07-22 15:24:18 +0200

    Fixed duplicate names.

--- PMA5EventsGBR/function_app.py
+++ PMA5EventsGBR/function_app.py
@@ -125,11 +125,11 @@ def EventsGBRFake(req: func.HttpRequest, context) -> func.HttpResponse:
 # Simulate the tasker
 @app.schedule(schedule="0 0 */1 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
-def TaskerFake(myTimer: func.TimerRequest) -> None:
+def SchedulerFiveMinutesFake(myTimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    logging.info('This is the SchedulerFiveMinutesFake timer job that ran at %s', utc_timestamp)
