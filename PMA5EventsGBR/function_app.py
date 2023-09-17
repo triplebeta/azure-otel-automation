3d49653f7002c69a3776c3de535602ec11ac4517
Jeroen <jeroenbijleveld@hotmail.com>
2023-09-17 09:48:03 +0200

    Added more telemetry

--- PMA5EventsGBR/function_app.py
+++ PMA5EventsGBR/function_app.py
@@ -3,35 +3,38 @@ import os
 import datetime
 import logging
 import random
-from time import sleep
 import azure.functions as func
 
 # For using the event hub
-from azure.eventhub import EventData, TransportType
+from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
 
 # Open Telemetry
 from azure.monitor.opentelemetry import configure_azure_monitor
 from opentelemetry import trace, metrics
-from  applicationinsights  import  TelemetryClient
+# from  applicationinsights  import  TelemetryClient
 
-# Workaround (part 1/3) for Azure Functions, according to: https://learn.microsoft.com/en-us/azure/azure-monitor/app/opentelemetry-python-opencensus-migrate
+# Workaround (part 1/3) specifically for Azure Functions, according to: https://learn.microsoft.com/en-us/azure/azure-monitor/app/opentelemetry-python-opencensus-migrate
 from opentelemetry.context import attach, detach
 from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 
 configure_azure_monitor()
 tracer = trace.get_tracer(__name__)
 
-# Added to enable manual registration of dependencies to complete the Application Map of AppInsights
-instrumentation_key = '1363bf29-376b-438f-b89b-52894213dd2a'
-tc = TelemetryClient(instrumentation_key)
-
-# Create metrics
+# Create metrics. unit must be one of the values from the UCUM, see: https://ucum.org/ucum
+# Types of telemetry, https://www.timescale.com/blog/a-deep-dive-into-open-telemetry-metrics/
+# The "observable" metrics are asynchronous. Important difference: they should NOT report delta values but absolute values. The framework will convert then them to deltas.
+# Counter: delta's, only increasing
+# Gauge: data you would not want to sum but might want to avg or use max.
 meter = metrics.get_meter_provider().get_meter(__name__)
-counterMachinesProcessed = meter.create_counter(name="MachinesProcessed",description="Count of batches processed for a machine.")
-counterEventsCreated = meter.create_counter(name="EventsCreated", description="Total number of events created for this machine.")
-counterEventCreationDuration = meter.create_counter(name="EventCreationDuration", description="Total duration for creating the events for this machine.")
+eventFilesProcessedCounter = meter.create_counter(name="MachinesProcessed", description="Count of batches processed for a machine.")
+validEventCounter = meter.create_counter(name="ValidEvents", description="Total number of valid events created for this machine.")
+invalidEventCounter = meter.create_counter(name="InvalidEvents", description="Total number of invalid events that were skipped.")
+processingTimeCounter = meter.create_counter(name="ProcessingTime", unit="second", description="Total duration for creating the events for this machine.")
+eventDayCoverage = meter.create_observable_gauge(name="EventDayCoverage", description="Percentage of the day for which we have full event coverage.")
 
+# Global variable to use in async callback for metric
+machinenr = "Unknown"  # Must be read from the request.
 
 app = func.FunctionApp()
 
@@ -43,11 +46,11 @@ async def EventsGBRFake(req: func.HttpRequest, context) -> func.HttpResponse:
         Events GBR simulation: pretents that a batch of events is processed
         Http POST should contain json to specify the details of the batch:
         - machinenr
-        - number of events
-        - package name
+        - number of lines
+        - event filename
     """
 
-    # Workaround (part 2/3)
+    # Workaround (part 2/3) to on context information.
     functions_current_context = {
         "traceparent": context.trace_context.Traceparent,
         "tracestate": context.trace_context.Tracestate
@@ -55,10 +58,14 @@ async def EventsGBRFake(req: func.HttpRequest, context) -> func.HttpResponse:
     parent_context = TraceContextTextMapPropagator().extract(carrier=functions_current_context)
     token = attach(parent_context)
 
-    # Register that this FunctionApp has a dependency on the AppService
-    with tracer.start_as_current_span("register dependency from appService on FunctionApp") as span:
-        tc.track_dependency(name="Events GBR (prod)", type="function_app", data=context.function_name, target="pma5poc-eventsgbr-app.azurewebsites.net", success=True, result_code=0)
-        tc.flush()
+    # Explicitly register that this FunctionApp has a dependency on the AppService
+    # Added to enable manual registration of dependencies to complete the Application Map of AppInsights
+    # instrumentation_key = '1363bf29-376b-438f-b89b-52894213dd2a'
+    # tc = TelemetryClient(instrumentation_key)
+    # with tracer.start_as_current_span("register dependency from appService on FunctionApp") as span:
+    #     tc.track_dependency(name="Events GBR (prod)", type="function_app", data=context.function_name, target="pma5poc-eventsgbr-app.azurewebsites.net", success=True, result_code=0)
+    #     tc.flush()
+
 
     # Extract the machinenr from the POST request
     # Read json to get the parameter values
@@ -70,97 +77,84 @@ async def EventsGBRFake(req: func.HttpRequest, context) -> func.HttpResponse:
             pass
         else:
             machinenr = req_body.get('machinenr')
-            numberOfEvents = int(req_body.get('numberOfEvents'))
-            packageName = req_body.get('packageName')
+            numberOfLines = int(req_body.get('numberOfLines'))
+            eventFilename = req_body.get('eventFilename')
 
-        if not machinenr or not numberOfEvents or not packageName:
-            return func.HttpResponse("Body must contain machinenr, numberOfEvents and packageName.", status_code=400)
+        if not machinenr or not numberOfLines or not eventFilename:
+            return func.HttpResponse("Body must contain machinenr, numberOfEvents and eventFilename.", status_code=400)
 
         # machinenr value wasn't known yet at the start of the span, so set it now
         span.set_attribute("machinenr",machinenr) # Support finding the span by the machinenr
-        logging.info(f"Processing {numberOfEvents} events for machine '{machinenr}' from package {packageName}.")
+        logging.info(f"Parsing {numberOfLines} lines of  file {eventFilename} for machine '{machinenr}'.")
     
-    try:
-        # Immediately pass additional info like machinenr to the span
-        with tracer.start_as_current_span("step 2. process the events", attributes={"machinenr":machinenr}):
-            if numberOfEvents == -1: 
-                raise Exception("Injected error -1: Less than 100 events received.")
+        try:
+            # Immediately pass additional info like machinenr to the span
+            with tracer.start_as_current_span("step 2. process the events", attributes={"machinenr":machinenr}):
+                if numberOfLines == -1: 
+                    raise Exception("Injected error -1: Less than 100 events received.")
 
-            if numberOfEvents == 200:
-                logging.warning("Injected warning -2: Some suspicious data found.")
+                if numberOfLines == 200:
+                    logging.warning("Injected warning -2: Some suspicious data found.")
 
-
-        # Proceed with normal handling of the events
-        # Wait for some seconds, based on the numberOfEvents.
-        # Waiting time is always a number of seconds.
-        # Example:
-        #   200 events --> 2 tot 4 seconden
-        #   900 events --> 9 tot 18 seconden
-        lowDuration = numberOfEvents // 100
-        processingDuration = random.randrange(lowDuration, lowDuration*2)
-        logging.info(f"GBR Event processing started (duration: {processingDuration}).")
-
-        # Send an event to the Event Hub
-        with tracer.start_as_current_span("send trigger for tasks to Event Hub"):
-            # Compose the name of the event hub to use for this environment
-            if os.environ['APP_CONFIGURATION_LABEL'] == 'production':
-                EVENT_HUB_NAME = 'eventsgbr'         # for production
-            else:
-                EVENT_HUB_NAME = 'eventsgbr-staging' # for testing
-            
-            EVENT_HUB_CONNECTION_STR = os.environ["EVENTHUB_CONNECTION_STRING"]
-            producer = EventHubProducerClient.from_connection_string(EVENT_HUB_CONNECTION_STR, eventhub_name=EVENT_HUB_NAME) # , transport_type=TransportType.AmqpOverWebsocket
-            
-            async with producer:
-                # event_data = "this is the first message"
-                event_data_batch = await producer.create_batch()
-                logging.debug("Event batch created.")
-
-                # Create the message for the Tasker.
-                # Some other samples:  'uv': random.random(), 'humidity': random.randint(70, 100)
-                taskerCmd = {'machinenr': machinenr, 'timestamp': str(datetime.datetime.utcnow()), 'numberOfEvents': numberOfEvents}
-                jsonTaskerCmd = json.dumps(taskerCmd) # Convert the reading into a JSON string.
-
-                # Send the command to the tasker
-                event_data_batch.add(EventData(jsonTaskerCmd))
-                logging.debug(f"Sending batch to event hub {EVENT_HUB_NAME}...")
-                await producer.send_batch(event_data_batch)
-                logging.info(f'Successfully delivered batch to event hub for machine {machinenr}.')
-
-        with tracer.start_as_current_span("compose metrics"):
-            logging.debug("Creating metrics...")
-
-            # Increase the machines_processed counter
-            counterMachinesProcessed.add(1.0, {"machinenr": machinenr})  # Each Http Trigger is for 1 machine
-            logging.debug("Created metric: MachinesProcessed")
 
             # Total number of events created for this machine
-            counterEventsCreated.add(numberOfEvents, {"machinenr": machinenr, "duration": processingDuration})
-            logging.debug("Created metric: EventsCreated")
+            # For testing we assume that for machine FQ99 1% of the events is invalid. All other eventfiles ar 100% ok.
+            if machinenr=="FQ99":
+                validEventsCount = numberOfLines * 0.99
+                invalidEventsCount = numberOfLines - validEventsCount
+            else:
+                validEventsCount = numberOfLines
+                invalidEventsCount = 0
 
-            # Total duration for creating the events for this machine.
-            counterEventCreationDuration.add(processingDuration, {"machinenr": machinenr})
-            logging.debug("Created metric: EventCreationDuration")
+            # Proceed with normal handling of the events
+            # Fake how long it takes to parse the events
+            lowDuration = numberOfLines // 100
+            processingDuration = random.randrange(lowDuration, lowDuration*2)
+            logging.info(f"GBR Event processing started.")
 
-        # Just send some response
-        return func.HttpResponse(f"Completed processing events for machine {machinenr}", status_code=200)
-    
-    finally:
-        # Workaround (part 3/3)
-        logging.debug("Detaching token...")
-        token = detach(token)
-        logging.debug("Detached token.")
+            # Send an event to the Event Hub
+            with tracer.start_as_current_span("Send trigger for tasks to Event Hub", attributes={"machinenr":machinenr}):
+                EVENT_HUB_CONNECTION_STR = os.environ["EVENTHUB_CONNECTION_STRING"]
+                producer = EventHubProducerClient.from_connection_string(EVENT_HUB_CONNECTION_STR, eventhub_name='eventsgbr') # , transport_type=TransportType.AmqpOverWebsocket
+                
+                async with producer:
+                    # Create the message for the Tasker with number of valid events.
+                    # In the metrics we track how many were valid / invalid.
+                    taskerCmd = {'machinenr': machinenr, 'timestamp': str(datetime.datetime.utcnow()), 'numberOfEvents': validEventsCount}
+                    jsonTaskerCmd = json.dumps(taskerCmd) # Convert the reading into a JSON string.
+
+                    # Send trigger to the tasker via Event Hub
+                    event_data_batch = await producer.create_batch()
+                    event_data_batch.add(EventData(jsonTaskerCmd))
+                    await producer.send_batch(event_data_batch)
+                    logging.info(f'Successfully delivered trigger for tasker to event hub for machine {machinenr}.')
+
+
+                logging.info(f'Creating events GBR metrics for machine {machinenr}.')
+                # Increase the machines_processed counter. Each Http Trigger processes 1 file of a machine.
+                eventFilesProcessedCounter.add(1.0, {"machinenr": machinenr, "eventFilename": eventFilename, "numberOfLines": numberOfLines})
+
+                # Total duration for processsing this file. Assuming each event is 1 line. Count lines of successful and failed processed events.
+                processingTimeCounter.add(processingDuration, {"machinenr": machinenr, "eventFilename": eventFilename, "numberOfLines": numberOfLines})
+
+                # Number of valid and invalid events
+                validEventCounter.add(validEventsCount, {"machinenr": machinenr, "eventFilename": eventFilename})
+                invalidEventCounter.add(invalidEventsCount, {"machinenr": machinenr, "eventFilename": eventFilename})
+
+            # Done with all of it
+            logging.info(f"GBR Event processing started (duration: {processingDuration}).")
+            return func.HttpResponse(f"Completed processing events for machine {machinenr}", status_code=200)
+        
+        finally:
+            # Workaround (part 3/3)
+            token = detach(token)
 
 
 
-# Time-triggered function, just for some testing.
-# @app.schedule(schedule="0 0 */1 * * *", arg_name="myTimer", run_on_startup=True,
-#               use_monitor=False) 
-# def SchedulerFiveMinutesFake(myTimer: func.TimerRequest) -> None:
-#     utc_timestamp = datetime.datetime.utcnow().replace(
-#         tzinfo=datetime.timezone.utc).isoformat()
-#
-#     if myTimer.past_due:
-#         logging.info('The timer is past due!')
-#
-#     logging.info('This is the SchedulerFiveMinutesFake timer job that ran at %s', utc_timestamp)
+# Event day coverage may go up or down. Use a callback function for Async version.
+def observable_up_down_counter_func():
+    # This reports the current value, which will be converted to a delta internally
+    # Get the coverage for this machine
+
+    #TODO Get the event file coverage here. Difficulty is that we can only compute this once everything is done.
+    return (33, {"machinenr": machinenr})
--- PMA5Tasks/function_app.py
+++ PMA5Tasks/function_app.py
@@ -1,7 +1,7 @@
 from typing import List
 import json
-import os
 import logging
+import random
 import azure.functions as func
 
 # For using the event hub
@@ -9,8 +9,7 @@ from azure.eventhub import EventData
 from azure.eventhub import EventHubConsumerClient
 
 from azure.monitor.opentelemetry import configure_azure_monitor
-from opentelemetry import trace
-from opentelemetry import metrics
+from opentelemetry import trace, metrics
 
 # Workaround (part 1/3) for Azure Functions, according to: https://learn.microsoft.com/en-us/azure/azure-monitor/app/opentelemetry-python-opencensus-migrate
 from opentelemetry.context import attach, detach
@@ -19,11 +18,11 @@ from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapProp
 configure_azure_monitor()
 tracer = trace.get_tracer(__name__)
 
-# Compose the name of the event hub to use for this environment
-if os.environ['APP_CONFIGURATION_LABEL'] == 'production':
-     ehName = 'eventsgbr'  # production
-else:
-     ehName = 'eventsgbr-staging'
+meter = metrics.get_meter_provider().get_meter(__name__)
+taskerTriggersProcessedSuccessfulCounter = meter.create_counter(name="TaskerRunSuccess", description="Count of successful runs for a machine.")
+tasksProducedCounter = meter.create_counter(name="TasksProduced", description="Total number of tasks created for this machine.")
+processingTimeCounter = meter.create_counter(name="ProcessingTime", unit="second", description="Total duration for creating the events for this machine.")
+
 
 app = func.FunctionApp()
 
@@ -31,10 +30,8 @@ app = func.FunctionApp()
 # Cardinality can be set to "many" or to "one".
 # It will log more metrics when using "many" 
 @app.function_name(name="TaskerFake")
-@app.event_hub_message_trigger(arg_name="myEvents", event_hub_name=ehName, cardinality="one", connection="EVENTHUB_CONNECTION_STRING") 
+@app.event_hub_message_trigger(arg_name="myEvents", event_hub_name='eventsgbr', cardinality="one", connection="EVENTHUB_CONNECTION_STRING") 
 def TaskerFake(myEvents: func.EventHubEvent, context):
-     logging.info(f'Listening to event hub {ehName}:')
-
      # Workaround (part 2/3)
      functions_current_context = {
           "traceparent": context.trace_context.Traceparent,
@@ -50,18 +47,33 @@ def TaskerFake(myEvents: func.EventHubEvent, context):
 
           # Collect the info from the message
           machinenr=msgObj["machinenr"]
-          numberOfEvents=int(msgObj["numberOfEvents"])
+          numberOfEvents=int(msgObj["numberOfEvents"])  # The number of valid
           timestamp=msgObj["timestamp"]
 
+          # Any tasker job with 330 valid events in it, will FAIL.
           if numberOfEvents == 330:
                raise Exception("Injected error 330: Tasker crashed.")
+          
+          # Create a fake number of tasks
+          numberOfTasks = (numberOfEvents / 2)  + 5
+          lowDuration = numberOfTasks // 100
+          processingDurationInSeconds = random.randrange(lowDuration, lowDuration*2)
 
           with tracer.start_as_current_span("receiving event and creating tasks") as span:
+               # Support finding the span by the machinenr
+               span.set_attribute("machinenr",machinenr)
+
                # Log info with some extra information in key-valye pairs
                logging.info(f'Function triggered to process for machine {machinenr}: {myEvents.get_body().decode("utf-8")} with SequenceNumber = {myEvents.sequence_number} and Offset = {myEvents.offset}', extra={"machinenr":machinenr})
 
-               # Support finding the span by the machinenr
-               span.set_attribute("machinenr",machinenr)
-     finally:
+
+          logging.info(f'Creating tasker metrics for machine {machinenr}.')
+          taskerTriggersProcessedSuccessfulCounter.add(1.0, {"machinenr": machinenr, "numberOfTasks": numberOfTasks})
+
+          tasksProducedCounter.add(numberOfTasks, {"machinenr": machinenr})
+          # Total duration for processsing this file. Assuming each event is 1 line. Count lines of successful and failed processed events.
+          processingTimeCounter.add(processingDurationInSeconds, {"machinenr": machinenr, "numberOfTasks": numberOfTasks})
+
+     finally:        
           # Workaround (part 3/3)
           token = detach(token)
