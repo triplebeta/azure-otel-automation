cd392180c5b59e4b59e13852a536419b7d15ffb1
Jeroen <jeroenbijleveld@hotmail.com>
2023-12-25 10:45:13 +0100

    Added observable counter and comments

--- PMA5FakeLogGenerator/function_app.py
+++ PMA5FakeLogGenerator/function_app.py
@@ -16,22 +16,55 @@ from opentelemetry.metrics import CallbackOptions, Observation
 from opentelemetry.context import attach, detach
 from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 
+# Enable telemetry for this Azure Function
+# TODO Pass a storage_directory for storing logs when offline
+# TODO Pass credential=ManagedIdentityCredential() to authenticate to ApplicationInsights
 configure_azure_monitor()
-tracer = trace.get_tracer(__name__)
+
+class Run:
+    def __init__(self, machinenr, batchId):
+        # Unique identifier for each run
+        self.runId = 'run-'+uuid.uuid4().hex
+        self.machinenr = machinenr
+
+        # Generate an it do use for correlating all runs.
+        self.batchId=batchId
+        if (self.batchId=="" or self.batchId is None):
+            self.is_new_batch=True
+            self.batchId = 'batch-' + uuid.uuid4().hex
+        else:
+            self.is_new_batch=False
+
+# Used sample from https://stackoverflow.com/questions/74804185/update-observablegauge-in-open-telemetry-python
+# and https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/monitor/azure-monitor-opentelemetry-exporter
+# Define the object to observe in the callback of the Gauge metric.
+class ParallelRunsTracker:
+    def __init__(self):
+        self.activeRuns = []
+
+    def register_start_run(self, run):
+        self.activeRuns.append(run)
+
+    def register_end_run(self, run):
+        self.activeRuns.remove(run)
+
+    def get_active_run_count(self):
+        return self.activeRuns.count
 
 
-# Used to pass context info to the observable counter (async)
-# global machinenr, runDuration
+# Not the best example of a gauge but it helps to show how to implement an observable metric
+# TODO Instead of 1 value, return an array with the number of parallel runs per machine
+def create_parallel_runs_observable_updown_counter(parallelRunsTracker):
+    """ Create an async UpDown counter """
+    def observable_updown_counter_func(options: CallbackOptions) -> Iterable[Observation]:
+        activeRunCount = parallelRunsTracker.get_active_run_count()
+        logging.info(f'Observable UpDown Counter: currently {activeRunCount} runs going on.')
+        yield Observation(activeRunCount)
 
-# Event day coverage may go up or down. Use a callback function for Async version.
-# def observable_gauge_runDuration_func(options: CallbackOptions) -> Iterable[Observation]:
-#     # This reports the current value, which will be converted to a delta internally
-#     # Get the coverage for this machine
-#     global machinenr, runDuration
+    return meter.create_observable_gauge("RunDuration", [observable_updown_counter_func],description="Number of parallel runs going on.")
 
-#     # Compute the duration
-#     logging.info(f'observable gauge: run for {machinenr} has a duration of {runDuration} seconds.')
-#     yield Observation(runDuration, {"machinenr": machinenr})
+# Create a object to keep track of the parallel runs so an observable UpDown metric can report on it
+parallelRunsTracker=ParallelRunsTracker()
 
 
 # Create metrics. unit must be one of the values from the UCUM, see: https://ucum.org/ucum
@@ -40,14 +73,18 @@ tracer = trace.get_tracer(__name__)
 # Counter: delta's, only increasing
 # Gauge: data you would not want to sum but might want to avg or use max.
 meter = metrics.get_meter_provider().get_meter(__name__)
-successfulBatchCounter = meter.create_counter(name="Batches", description="Count of batches processed for a machine.")
+successfulBatchCounter = meter.create_counter(name="tasks.batches", description="Count of batches processed for a machine.")
 runCounter = meter.create_counter(name="Runs", description="Count all runs for a machine, incl. retries.")
 retriedRunCounter = meter.create_counter(name="RetriedRuns", description="Count only of retry-runs for a machine.")
 failedRunCounter = meter.create_counter(name="FailedRuns", description="Count failed runs for a machine.")
 completedRunCounter = meter.create_counter(name="CompletedRuns", description="Count successfully completed runs for a machine.")
 manualRunCounter = meter.create_counter(name="ManualRuns", description="Count manual runs for a machine.")
-# runDurationCounter = meter.create_observable_gauge("RunDuration", callbacks=[observable_gauge_runDuration_func], description="")
+parallelRunsUpDownCounter = meter.create_counter(name="ParallelRuns", description="Number of jobs running in parallel.")
+create_parallel_runs_observable_updown_counter(parallelRunsTracker)
 
+
+# Create a tracer
+tracer = trace.get_tracer(__name__)
 
 app = func.FunctionApp()
 @app.function_name(name="pma5poc-loggen-app")
@@ -57,13 +94,7 @@ def FakeLogGenerator(req: func.HttpRequest, context) -> func.HttpResponse:
         Generates fake log and telemetry entries.
     """
 
-    # Use the global machinenr
-    # global machinenr, runDuration
-
-    # # Initialize, otherwise the callback for the gauge will fail.
-    # machinenr=''
-    # runDuration=0
-
+    # TODO Check if this is still necessary or that it was only needed in early versions.
     # Workaround (part 2/3) to on context information.
     functions_current_context = {
         "traceparent": context.trace_context.Traceparent,
@@ -90,59 +121,56 @@ def FakeLogGenerator(req: func.HttpRequest, context) -> func.HttpResponse:
             batchId = req_body.get('batchId')                   # Optional, only for retries: Id of the batch 
             
             if (req_body.get('runNr') is not None):
-                runNr = int(req_body.get('runNr'))            # 1 for first run, 2 for first retry etc...
+                runNr = int(req_body.get('runNr'))              # 1 for first run, 2 for first retry etc...
             else: runNr=1
 
         if not machinenr or not processName or not runDuration:
             return func.HttpResponse("Body must contain machinenr, runDuration, isSuccessful, isManualRun, batchId, runNr and processName.", status_code=400)
 
-
     with tracer.start_as_current_span("Tasker execution") as span:
         # You can also set an attribute for a span after its creation
         span.set_attribute("machinenr",machinenr) # Support finding the span by the machinenr
 
-        # Generate an it do use for correlating all runs.
-        isNewBatch=(batchId=="" or batchId is None)
-        if (isNewBatch): batchId = 'batch-' + uuid.uuid4().hex
-
-        # Unique identifier for each run
-        runId = 'run-'+uuid.uuid4().hex
+        # Create a new run
+        activeRun = Run(machinenr, batchId)
 
         # Return the generated IDs so they can be used in subsequent Postman requests.
         # This is just useful to make the requests more realistic.
-        logMetadata={"machinenr":machinenr, "batchId":batchId, "runId":runId, "runNr":runNr}
+        runLabel = str(runNr)
+        logMetadata={"machinenr":machinenr, "batchId":activeRun.batchId, "runId":activeRun.runId, "runNr":runNr}
         if (isManualRun):
             logMetadata["isManualRun"]=isManualRun  # Only add this if it's a manual run 
-            manualRunCounter.add(1,{"machinenr":machinenr,"batchId":batchId})
+            manualRunCounter.add(1,{"machinenr":machinenr,"batchId":activeRun.batchId})
+            runLabel = "manual" 
 
         # For now: simply use the same structure to return in the HTTP Response
         httpResponse=logMetadata 
         # Immediately pass additional info like machinenr to the span
         with tracer.start_as_current_span("Fake process logic", record_exception=False, attributes=logMetadata):
             try:
+                # Track the start of a new run
+                parallelRunsTracker.register_start_run(activeRun)
+
                 # Log start of the batch (which can consist of this one and possible retry runs)
-                if (isNewBatch):
+                if (activeRun.is_new_batch):
                     logging.info(f'{processName} started batch', extra=logMetadata)
-                    successfulBatchCounter.add(1,{"machinenr":machinenr,"batchId":batchId})
+                    successfulBatchCounter.add(1,{"machinenr":machinenr,"batchId":activeRun.batchId})
 
                 # Show the information also in the log text, as well as in the dimensions
                 if (runNr==1):
-                    logging.info(f'{processName} started run ({runNr})', extra=logMetadata)
-                    runCounter.add(1,{"machinenr":machinenr,"batchId":batchId, "runId":runId})
+                    logging.info(f'{processName} started run ({runLabel})', extra=logMetadata)
+                    runCounter.add(1,attributes=logMetadata)
                 else:
-                    logging.info(f'{processName} start retry ({runNr})', extra=logMetadata)
-                    retriedRunCounter.add(1,{"machinenr":machinenr,"batchId":batchId, "runId":runId})
+                    logging.info(f'{processName} start retry ({runLabel})', extra=logMetadata)
+                    retriedRunCounter.add(1,attributes=logMetadata)
 
                 # Simulate processing time
                 time.sleep(runDuration)
 
                 # Also report the duration
                 if (isSuccessful):
-                    completedRunCounter.add(1,{"machinenr":machinenr,"batchId":batchId, "runId":runId})
-                    if (isManualRun):
-                        logging.info(f'{processName} completed run (manual)',extra=logMetadata)
-                    else:
-                        logging.info(f'{processName} completed run ({runNr})',extra=logMetadata)
+                    completedRunCounter.add(1,attributes=logMetadata)
+                    logging.info(f'{processName} completed run ({runLabel})',extra=logMetadata)
                 else:
                     # Simulate that something aborts the process
                     raise Exception(f'Some fake exception for {processName}')
@@ -151,10 +179,16 @@ def FakeLogGenerator(req: func.HttpRequest, context) -> func.HttpResponse:
                 return func.HttpResponse(json.dumps(httpResponse), mimetype="application/json", status_code=200)
             
             except Exception as error:
-                # Log the exception and register that the run failed
-                logging.exception(error, extra=logMetadata)
-                logging.error(f'{processName} failed run ({runNr})',extra=logMetadata)
-                failedRunCounter.add(1,{"machinenr":machinenr,"batchId":batchId, "runId":runId})
+                # Log the exception (which includes the traceback)
+                # On the span, set record_exception=False because we handle it here and include more info
+                logging.exception(f'{processName} error handled!',exc_info=error, extra=logMetadata)
+
+                # And log a more simple error message
+                logging.error(f'{processName} failed run ({runLabel})',extra=logMetadata)
+                failedRunCounter.add(1,attributes=logMetadata)
 
                 # Indicate it failed, return 500 (Server error)
                 return func.HttpResponse(json.dumps(httpResponse), mimetype="application/json", status_code=500)
+            finally:
+                # Register the end of the run
+                parallelRunsTracker.register_end_run(activeRun)
